411b8e075d004e2fd64e0385ba670b178c7ee033
vita-ride <viktornovakovic77@gmail.com>
2022-04-10 21:01:41 +0200

    Add starting popup rectangle
    Clicking the button opens the rectangle,
    clicking again closes the rectangle.

---
+++ Shooter/Assets/Scripts/PopupButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupButton : MonoBehaviour
+{
+    [SerializeField] Popup popup;
+
+    public void click(){
+        GameObject popupObj = popup.gameObject;
+        if (popupObj.activeInHierarchy){
+            popup.gameObject.SetActive(false);
+        }
+        else {
+            popup.gameObject.SetActive(true);
+        }
+        
+    }
+
+}
